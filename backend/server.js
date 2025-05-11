669b4dd78c54a79cbfe1e0ed7140174cae2f4939
FM Fuad <fartinfuad@gmail.com>
2025-05-11 16:38:10 +0600

    ..

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
     credentials: true,
 }));
 app.use(cookieParser());
