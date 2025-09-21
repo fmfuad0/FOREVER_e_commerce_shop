9bcb556265f16c67926be98c0d270dda8b0bb0d1
Md. Fartin Mahtadi Fuad <111303366+fmfuad0@users.noreply.github.com>
2025-09-21 06:36:02 +0600

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -62,7 +62,7 @@ function Login(props) {
                 </div>
 
                 <button onClick={onSubmit} type={"submit"} className={`text-semibold bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer tracking-[5px]`} >{currentState.toUpperCase()}</button>
-                Click login to enter.Username and password are not needed.<br/>First login may take some time.Pleas click login and wait or come back after 15 to 20 sec .
+                <span className="text-red-900">Click login to enter.</span><br/><span className="text-green-900">Username and password are not needed.</span><br/>First login may take some time.Please click login and wait or come back after 15 to 20 sec .
             </form>
 
         </div>
